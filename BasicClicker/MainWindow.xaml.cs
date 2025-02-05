27a3a19ce8fb4e8e40390c7126d00e197089a474
averyden <igo408gt@gmail.com>
2025-02-05 08:02:55 +0100

    Setup the keybind

--- BasicClicker/MainWindow.xaml.cs
+++ BasicClicker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Threading;
 
 namespace BasicClicker
@@ -15,9 +16,19 @@ namespace BasicClicker
         [DllImport("user32.dll", SetLastError = true)]
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
 
+        private const uint MOUSEEVENT_LEFTDOWN = 0x02;
+        private const uint MOUSEEVENT_RIGHTDOWN = 0x04;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // TODO: make a setting to allow the user to change their keybind.
+            var keyBind = new KeyGesture(Key.F6);
+            var command = new RoutedCommand();
+            command.InputGestures.Add(keyBind);
+            //CommandBindings.Add(new CommandBinding(command, whatever we call the method later on));
+
         }
     }
 }
