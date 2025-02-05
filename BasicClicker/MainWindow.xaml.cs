f663c4801d8a3406ab3796154d8da82b44f8876a
averyden <igo408gt@gmail.com>
2025-02-05 08:09:08 +0100

    Rename the rightdownEvent to be the actual event that it actually is

--- BasicClicker/MainWindow.xaml.cs
+++ BasicClicker/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace BasicClicker
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
 
         private const uint MOUSEEVENT_LEFTDOWN = 0x02;
-        private const uint MOUSEEVENT_RIGHTDOWN = 0x04;
+        private const uint MOUSEEVENTF_LEFTUP = 0x04;
 
         public MainWindow()
         {
@@ -50,6 +50,8 @@ namespace BasicClicker
 
         private void actuallyClick()
         {
+            mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, IntPtr.Zero);
+            Thread.Sleep(10);
 
         }
     }
