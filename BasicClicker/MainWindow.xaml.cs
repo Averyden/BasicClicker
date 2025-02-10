15470d024ce10db811f2393d3012c21b463c3217
averyden <igo408gt@gmail.com>
2025-02-10 15:01:44 +0100

    Modify return types of registration methods to return a bool instead of nothing when called

--- BasicClicker/MainWindow.xaml.cs
+++ BasicClicker/MainWindow.xaml.cs
@@ -17,10 +17,10 @@ namespace BasicClicker
         private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo);
 
         [DllImport("user32.dll", SetLastError = true)]
-        private static extern void registerHotkey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        private static extern bool registerHotkey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
 
         [DllImport("user32.dll", SetLastError = true)]
-        private static extern void unregisterHotkey(IntPtr hWnd, int id);
+        private static extern bool unregisterHotkey(IntPtr hWnd, int id);
 
 
         private const uint MOUSEEVENT_LEFTDOWN = 0x02;
